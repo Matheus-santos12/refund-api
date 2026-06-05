987608c8713ecaebc8479e187b4ab49702e8ed11
Matheus Silva <rematheussilva@gmail.com>
2026-06-04 21:09:45 -0500

    feat: add handle exceptions

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,12 @@
 import cors from "cors";
 import express from "express";
 
+import { errorHandling } from "./middlewares/error-handling";
+
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Hello, World!");
-});
+app.use(errorHandling);
 
 export { app };
