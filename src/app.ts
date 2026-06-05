5ba413cc3e724c02b28425edeb3e633c2b8dc2ba
Matheus Silva <rematheussilva@gmail.com>
2026-06-05 16:14:15 -0500

    feat: add route and controller for creating user

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,13 @@ import cors from "cors";
 import express from "express";
 
 import { errorHandling } from "./middlewares/error-handling";
+import { routes } from "./routes";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use(routes);
 app.use(errorHandling);
 
 export { app };
