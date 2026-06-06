51b151ab4bf8a1fdf1a207408d81cb562ee5054f
Matheus Silva <rematheussilva@gmail.com>
2026-06-05 20:36:08 -0500

    feat: create authentication controller

---
+++ src/controllers/sessions-controller.ts
@@ -0,0 +1,7 @@
+import { Request, Response } from "express";
+
+export class SessionsController {
+  async create(request: Request, response: Response) {
+    response.json({ message: "OK!" });
+  }
+}
