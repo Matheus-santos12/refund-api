aa15532dbc1472f61a0c7e2a15a913b462438bdc
Matheus Silva <rematheussilva@gmail.com>
2026-06-09 17:05:02 -0500

    feat: add display request by id

--- src/routes/refunds-routes.ts
+++ src/routes/refunds-routes.ts
@@ -11,10 +11,17 @@ refundsRoutes.post(
   verifyUserAuthorization(["employee"]),
   refundsController.create,
 );
+
 refundsRoutes.get(
   "/",
   verifyUserAuthorization(["manager"]),
   refundsController.index,
+);
+
+refundsRoutes.get(
+  "/:id",
+  verifyUserAuthorization(["employee", "manager"]),
+  refundsController.show,
 );
 
 export { refundsRoutes };
