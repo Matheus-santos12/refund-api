72db9fbec25719daa2d3543118570cbf9a7001b6
Matheus Silva <rematheussilva@gmail.com>
2026-06-10 15:43:22 -0500

    feat: add route to view the file

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,15 @@ import cors from "cors";
 import express from "express";
 import "express-async-errors";
 
+import upload from "./configs/upload";
 import { errorHandling } from "./middlewares/error-handling";
 import { routes } from "./routes";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+app.use("/uploads", express.static(upload.UPLOADS_FOLDER));
 
 app.use(routes);
 app.use(errorHandling);
