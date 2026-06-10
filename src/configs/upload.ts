706cfb7f463a3a72533aa98c748a30fcea86989d
Matheus Silva <rematheussilva@gmail.com>
2026-06-10 14:34:15 -0500

    feat: add validate files during upload

--- src/configs/upload.ts
+++ src/configs/upload.ts
@@ -6,10 +6,11 @@ import { fileURLToPath } from "node:url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+const MAX_SIZE = 3;
 const TMP_FOLDER = path.resolve(__dirname, "..", "..", "tmp");
 const UPLOADS_FOLDER = path.relative(TMP_FOLDER, "uploads");
 
-const MAX_FILE_SIZE = 1024 * 1024 * 3;
+const MAX_FILE_SIZE = 1024 * 1024 * MAX_SIZE;
 const ACCEPTED_IMAGE_TYPES = ["image/jpeg", "image/jpg", "image/png"];
 
 const MULTER = {
@@ -27,6 +28,7 @@ const MULTER = {
 export default {
   ACCEPTED_IMAGE_TYPES,
   MAX_FILE_SIZE,
+  MAX_SIZE,
   MULTER,
   TMP_FOLDER,
   UPLOADS_FOLDER,
--- src/controllers/uploads-controller.ts
+++ src/controllers/uploads-controller.ts
@@ -1,7 +1,32 @@
+import upload from "@/configs/upload";
 import { Request, Response } from "express";
+import z from "zod";
 
 export class UploadsController {
   async create(request: Request, response: Response) {
-    response.json({ message: "ok" });
+    try {
+      const fileSchema = z
+        .object({
+          filename: z.string().min(1, "Arquivo é obrigatório"),
+          mimetype: z
+            .string()
+            .refine(
+              (type) => upload.ACCEPTED_IMAGE_TYPES.includes(type),
+              `Formato de arquivo inválido. Formatos permitidos: ${upload.ACCEPTED_IMAGE_TYPES}`,
+            ),
+          size: z
+            .number()
+            .positive()
+            .refine(
+              (size) => size <= upload.MAX_SIZE,
+              `Arquivo excede o tamanho máximo de ${upload.MAX_SIZE}`,
+            ),
+        })
+        .passthrough();
+
+      const { file } = fileSchema.parse(request.file);
+    } catch (error) {
+      throw error;
+    }
   }
 }
